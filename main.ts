d0bcb56dc0c178d669accde2aa120919de0d5625
bmarslandCN <69921936+bmarslandCN@users.noreply.github.com>
2021-07-07 10:11:33 -0700

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,156 @@
+namespace SpriteKind {
+    export const goal = SpriteKind.create()
+}
+info.onCountdownEnd(function () {
+    game.over(true)
+})
+sprites.onOverlap(SpriteKind.Enemy, SpriteKind.Food, function (sprite, otherSprite) {
+    info.changeLifeBy(-1)
+    pizza.setPosition(69, 20)
+    girl.setPosition(20, 100)
+    boy.setPosition(134, 98)
+    boy.follow(pizza, 50)
+    pizza.setImage(img`
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . f f f . f f f . . . . 
+        . . . . f 3 3 3 f 3 3 3 f . . . 
+        . . . . f 3 3 3 3 3 1 3 f . . . 
+        . . . . f 3 3 3 3 3 3 3 f . . . 
+        . . . . . f 3 b b b 3 f . . . . 
+        . . . . . f f b b b f f . . . . 
+        . . . . . . f f b f f . . . . . 
+        . . . . . . . f f f . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        `)
+    music.jumpDown.playUntilDone()
+    pizza.setImage(img`
+        . . . . . . b b b b . . . . . . 
+        . . . . . . b 4 4 4 b . . . . . 
+        . . . . . . b b 4 4 4 b . . . . 
+        . . . . . b 4 b b b 4 4 b . . . 
+        . . . . b d 5 5 5 4 b 4 4 b . . 
+        . . . . b 3 2 3 5 5 4 e 4 4 b . 
+        . . . b d 2 2 2 5 7 5 4 e 4 4 e 
+        . . . b 5 3 2 3 5 5 5 5 e e e e 
+        . . b d 7 5 5 5 3 2 3 5 5 e e e 
+        . . b 5 5 5 5 5 2 2 2 5 5 d e e 
+        . b 3 2 3 5 7 5 3 2 3 5 d d e 4 
+        . b 2 2 2 5 5 5 5 5 5 d d e 4 . 
+        b d 3 2 d 5 5 5 d d d 4 4 . . . 
+        b 5 5 5 5 d d 4 4 4 4 . . . . . 
+        4 d d d 4 4 4 . . . . . . . . . 
+        4 4 4 4 . . . . . . . . . . . . 
+        `)
+})
+info.onLifeZero(function () {
+    game.over(false)
+})
+sprites.onOverlap(SpriteKind.Player, SpriteKind.Food, function (sprite, otherSprite) {
+    info.changeScoreBy(1)
+    pizza.setPosition(69, 20)
+    girl.setPosition(20, 100)
+    boy.setPosition(134, 98)
+    boy.follow(pizza, 50)
+    pizza.setImage(img`
+        . . b b b b . . 
+        . b 5 5 5 5 b . 
+        b 5 d 3 3 d 5 b 
+        b 5 3 5 5 1 5 b 
+        c 5 3 5 5 1 d c 
+        c d d 1 1 d d c 
+        . f d d d d f . 
+        . . f f f f . . 
+        `)
+    music.jumpUp.playUntilDone()
+    pizza.setImage(img`
+        . . . . . . b b b b . . . . . . 
+        . . . . . . b 4 4 4 b . . . . . 
+        . . . . . . b b 4 4 4 b . . . . 
+        . . . . . b 4 b b b 4 4 b . . . 
+        . . . . b d 5 5 5 4 b 4 4 b . . 
+        . . . . b 3 2 3 5 5 4 e 4 4 b . 
+        . . . b d 2 2 2 5 7 5 4 e 4 4 e 
+        . . . b 5 3 2 3 5 5 5 5 e e e e 
+        . . b d 7 5 5 5 3 2 3 5 5 e e e 
+        . . b 5 5 5 5 5 2 2 2 5 5 d e e 
+        . b 3 2 3 5 7 5 3 2 3 5 d d e 4 
+        . b 2 2 2 5 5 5 5 5 5 d d e 4 . 
+        b d 3 2 d 5 5 5 d d d 4 4 . . . 
+        b 5 5 5 5 d d 4 4 4 4 . . . . . 
+        4 d d d 4 4 4 . . . . . . . . . 
+        4 4 4 4 . . . . . . . . . . . . 
+        `)
+})
+let boy: Sprite = null
+let girl: Sprite = null
+let pizza: Sprite = null
+scene.setBackgroundColor(1)
+pizza = sprites.create(img`
+    . . . . . . b b b b . . . . . . 
+    . . . . . . b 4 4 4 b . . . . . 
+    . . . . . . b b 4 4 4 b . . . . 
+    . . . . . b 4 b b b 4 4 b . . . 
+    . . . . b d 5 5 5 4 b 4 4 b . . 
+    . . . . b 3 2 3 5 5 4 e 4 4 b . 
+    . . . b d 2 2 2 5 7 5 4 e 4 4 e 
+    . . . b 5 3 2 3 5 5 5 5 e e e e 
+    . . b d 7 5 5 5 3 2 3 5 5 e e e 
+    . . b 5 5 5 5 5 2 2 2 5 5 d e e 
+    . b 3 2 3 5 7 5 3 2 3 5 d d e 4 
+    . b 2 2 2 5 5 5 5 5 5 d d e 4 . 
+    b d 3 2 d 5 5 5 d d d 4 4 . . . 
+    b 5 5 5 5 d d 4 4 4 4 . . . . . 
+    4 d d d 4 4 4 . . . . . . . . . 
+    4 4 4 4 . . . . . . . . . . . . 
+    `, SpriteKind.Food)
+pizza.setPosition(69, 20)
+girl = sprites.create(img`
+    . . . . . . . . . . . . . . 
+    . f f f . f f f f f . . . . 
+    f f f f f c c c c f f . . . 
+    f f f f b c c c c c c f . . 
+    f f f c 3 c c c c c c f . . 
+    . f 3 3 c c c c c c c c f . 
+    . f f f c c c c c 4 c c f . 
+    . f f f f c c c 4 4 c f f . 
+    . f f 4 4 f b f 4 4 f f f . 
+    . . f 4 d 4 1 f d d f f . . 
+    . . f f f e e d d d f . . . 
+    . . . f 4 d d e 4 e f . . . 
+    . . . f e d d e 3 3 f . . . 
+    . . f f f e e f 6 6 f f . . 
+    . . f f f f f f f f f f . . 
+    . . . f f . . . f f f . . . 
+    `, SpriteKind.Player)
+girl.setPosition(20, 100)
+controller.moveSprite(girl)
+girl.setStayInScreen(true)
+boy = sprites.create(img`
+    . . . . . . . . . . . . . 
+    . . . . f f f f f f . . . 
+    . . . f f f f f f f f f . 
+    . . f f f c f f f f f f . 
+    . f f f c f f f c f f f f 
+    f f c c f f f c c f f c f 
+    f f f f f e f f f f c c f 
+    . f f f e e f f f f f f f 
+    . . f f e e f b f e e f f 
+    . . f f 4 4 f 1 e 4 e f . 
+    . . . f 4 4 4 e e f f f . 
+    . . . f f e e 4 4 e f . . 
+    . . . f 7 7 e 4 4 e f . . 
+    . . f f 6 6 f e e f f f . 
+    . . f f f f f f f f f f . 
+    . . . f f f . . . f f . . 
+    `, SpriteKind.Enemy)
+boy.setPosition(134, 98)
+boy.follow(pizza, 50)
+info.setScore(0)
+info.setLife(3)
+info.startCountdown(10)
