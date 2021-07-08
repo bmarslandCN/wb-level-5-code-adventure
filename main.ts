4d79c811d167809a094379ba94deaa871b94a0eb
bmarslandCN <69921936+bmarslandCN@users.noreply.github.com>
2021-07-08 10:32:14 -0700

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -5,167 +5,200 @@ info.onCountdownEnd(function () {
     game.over(true)
 })
 sprites.onOverlap(SpriteKind.Enemy, SpriteKind.Food, function (sprite, otherSprite) {
-    info.changeLifeBy(-1)
-    sprite.destroy()
-    pizza.setImage(img`
-        . . . . . . . . . . . . . . . . 
-        . . . . . . . . . . . . . . . . 
-        . . . . . . . . . . . . . . . . 
-        . . . . . . . . . . . . . . . . 
-        . . . . . . . . . . . . . . . . 
-        . . . . . f f f . f f f . . . . 
-        . . . . f 3 3 3 f 3 3 3 f . . . 
-        . . . . f 3 3 3 3 3 1 3 f . . . 
-        . . . . f 3 3 3 3 3 3 3 f . . . 
-        . . . . . f 3 b b b 3 f . . . . 
-        . . . . . f f b b b f f . . . . 
-        . . . . . . f f b f f . . . . . 
-        . . . . . . . f f f . . . . . . 
-        . . . . . . . . . . . . . . . . 
-        . . . . . . . . . . . . . . . . 
-        . . . . . . . . . . . . . . . . 
-        `)
+    info.changeScoreBy(-1)
+    otherSprite.destroy()
     music.jumpDown.playUntilDone()
-    pizza.setImage(img`
-        . . . . . . b b b b . . . . . . 
-        . . . . . . b 4 4 4 b . . . . . 
-        . . . . . . b b 4 4 4 b . . . . 
-        . . . . . b 4 b b b 4 4 b . . . 
-        . . . . b d 5 5 5 4 b 4 4 b . . 
-        . . . . b 3 2 3 5 5 4 e 4 4 b . 
-        . . . b d 2 2 2 5 7 5 4 e 4 4 e 
-        . . . b 5 3 2 3 5 5 5 5 e e e e 
-        . . b d 7 5 5 5 3 2 3 5 5 e e e 
-        . . b 5 5 5 5 5 2 2 2 5 5 d e e 
-        . b 3 2 3 5 7 5 3 2 3 5 d d e 4 
-        . b 2 2 2 5 5 5 5 5 5 d d e 4 . 
-        b d 3 2 d 5 5 5 d d d 4 4 . . . 
-        b 5 5 5 5 d d 4 4 4 4 . . . . . 
-        4 d d d 4 4 4 . . . . . . . . . 
-        4 4 4 4 . . . . . . . . . . . . 
-        `)
 })
 info.onLifeZero(function () {
     game.over(false)
 })
 sprites.onOverlap(SpriteKind.Player, SpriteKind.Food, function (sprite, otherSprite) {
     info.changeScoreBy(1)
-    player1.setPosition(80, 60)
-    pizza.setImage(img`
-        . . b b b b . . 
-        . b 5 5 5 5 b . 
-        b 5 d 3 3 d 5 b 
-        b 5 3 5 5 1 5 b 
-        c 5 3 5 5 1 d c 
-        c d d 1 1 d d c 
-        . f d d d d f . 
-        . . f f f f . . 
-        `)
+    otherSprite.destroy()
     music.jumpUp.playUntilDone()
-    pizza.setImage(img`
-        . . . . . . b b b b . . . . . . 
-        . . . . . . b 4 4 4 b . . . . . 
-        . . . . . . b b 4 4 4 b . . . . 
-        . . . . . b 4 b b b 4 4 b . . . 
-        . . . . b d 5 5 5 4 b 4 4 b . . 
-        . . . . b 3 2 3 5 5 4 e 4 4 b . 
-        . . . b d 2 2 2 5 7 5 4 e 4 4 e 
-        . . . b 5 3 2 3 5 5 5 5 e e e e 
-        . . b d 7 5 5 5 3 2 3 5 5 e e e 
-        . . b 5 5 5 5 5 2 2 2 5 5 d e e 
-        . b 3 2 3 5 7 5 3 2 3 5 d d e 4 
-        . b 2 2 2 5 5 5 5 5 5 d d e 4 . 
-        b d 3 2 d 5 5 5 d d d 4 4 . . . 
-        b 5 5 5 5 d d 4 4 4 4 . . . . . 
-        4 d d d 4 4 4 . . . . . . . . . 
-        4 4 4 4 . . . . . . . . . . . . 
-        `)
 })
-let player1: Sprite = null
-let pizza: Sprite = null
-scene.setBackgroundColor(14)
-game.showLongText("Earn points by getting to the pizza first!", DialogLayout.Center)
-game.showLongText("Lose a life when the enemy beats you to the pizza.", DialogLayout.Center)
-pizza = sprites.create(img`
-    . . . . . . b b b b . . . . . . 
-    . . . . . . b 4 4 4 b . . . . . 
-    . . . . . . b b 4 4 4 b . . . . 
-    . . . . . b 4 b b b 4 4 b . . . 
-    . . . . b d 5 5 5 4 b 4 4 b . . 
-    . . . . b 3 2 3 5 5 4 e 4 4 b . 
-    . . . b d 2 2 2 5 7 5 4 e 4 4 e 
-    . . . b 5 3 2 3 5 5 5 5 e e e e 
-    . . b d 7 5 5 5 3 2 3 5 5 e e e 
-    . . b 5 5 5 5 5 2 2 2 5 5 d e e 
-    . b 3 2 3 5 7 5 3 2 3 5 d d e 4 
-    . b 2 2 2 5 5 5 5 5 5 d d e 4 . 
-    b d 3 2 d 5 5 5 d d d 4 4 . . . 
-    b 5 5 5 5 d d 4 4 4 4 . . . . . 
-    4 d d d 4 4 4 . . . . . . . . . 
-    4 4 4 4 . . . . . . . . . . . . 
-    `, SpriteKind.Food)
-pizza.setPosition(80, 60)
-player1 = sprites.create(img`
-    . . . . . . . . . . . . . . 
-    . f f f . f f f f f . . . . 
-    f f f f f c c c c f f . . . 
-    f f f f b c c c c c c f . . 
-    f f f c 3 c c c c c c f . . 
-    . f 3 3 c c c c c c c c f . 
-    . f f f c c c c c 4 c c f . 
-    . f f f f c c c 4 4 c f f . 
-    . f f 4 4 f b f 4 4 f f f . 
-    . . f 4 d 4 1 f d d f f . . 
-    . . f f f e e d d d f . . . 
-    . . . f 4 d d e 4 e f . . . 
-    . . . f e d d e 3 3 f . . . 
-    . . f f f e e f 6 6 f f . . 
-    . . f f f f f f f f f f . . 
-    . . . f f . . . f f f . . . 
-    `, SpriteKind.Player)
-player1.setPosition(80, 108)
-controller.moveSprite(player1)
-player1.setStayInScreen(true)
-let enemy1 = sprites.create(img`
-    . . . . . . . . . . . . . 
-    . . . . f f f f f f . . . 
-    . . . f f f f f f f f f . 
-    . . f f f c f f f f f f . 
-    . f f f c f f f c f f f f 
-    f f c c f f f c c f f c f 
-    f f f f f e f f f f c c f 
-    . f f f e e f f f f f f f 
-    . . f f e e f b f e e f f 
-    . . f f 4 4 f 1 e 4 e f . 
-    . . . f 4 4 4 e e f f f . 
-    . . . f f e e 4 4 e f . . 
-    . . . f 7 7 e 4 4 e f . . 
-    . . f f 6 6 f e e f f f . 
-    . . f f f f f f f f f f . 
-    . . . f f f . . . f f . . 
-    `, SpriteKind.Enemy)
-enemy1.setPosition(140, 100)
-enemy1.follow(player1, 15)
-let enemy2 = sprites.create(img`
-    . . . . . . . . . . . . 
-    . . . f f f f f f . . . 
-    . f f f e e e e e f . . 
-    f f f e e e e e e e f . 
-    f f f f e e e e e e e f 
-    f f f f f e e e 4 e e f 
-    f f f f e e e 4 4 e e f 
-    f f f f 4 4 4 4 4 e f f 
-    f f 4 e 4 f f 4 4 e f . 
-    f f 4 d 4 d d d d f . . 
-    . f f f 4 d d b b f . . 
-    . 4 d d e 4 4 4 e f . . 
-    . e d d e 1 1 1 1 f . . 
-    . f e e f 6 6 6 6 f f . 
-    . f f f f f f f f f f . 
-    . . f f . . . f f f . . 
-    `, SpriteKind.Enemy)
-enemy2.setPosition(20, 100)
-enemy2.follow(player1, 25)
+sprites.onOverlap(SpriteKind.Player, SpriteKind.Enemy, function (sprite, otherSprite) {
+    info.changeLifeBy(-1)
+    music.powerDown.playUntilDone()
+})
 info.setScore(0)
 info.setLife(3)
-info.startCountdown(10)
+info.startCountdown(8)
+scene.setBackgroundColor(3)
+let myPlayer = sprites.create(img`
+    . . . . f f f f . . . . 
+    . . f f e e e e f f . . 
+    . f f e e e e e e f f . 
+    f f f f 4 e e e f f f f 
+    f f f 4 4 4 e e f f f f 
+    f f f 4 4 4 4 e e f f f 
+    f 4 e 4 4 4 4 4 4 e 4 f 
+    f 4 4 f f 4 4 f f 4 4 f 
+    f e 4 d d d d d d 4 e f 
+    . f e d d b b d d e f . 
+    . f f e 4 4 4 4 e f f . 
+    e 4 f b 1 1 1 1 b f 4 e 
+    4 d f 1 1 1 1 1 1 f d 4 
+    4 4 f 6 6 6 6 6 6 f 4 4 
+    . . . f f f f f f . . . 
+    . . . f f . . f f . . . 
+    `, SpriteKind.Player)
+myPlayer.setPosition(80, 10)
+controller.moveSprite(myPlayer)
+myPlayer.setStayInScreen(true)
+let food1 = sprites.create(img`
+    . . . . . . . e c 7 . . . . . . 
+    . . . . e e e c 7 7 e e . . . . 
+    . . c e e e e c 7 e 2 2 e e . . 
+    . c e e e e e c 6 e e 2 2 2 e . 
+    . c e e e 2 e c c 2 4 5 4 2 e . 
+    c e e e 2 2 2 2 2 2 4 5 5 2 2 e 
+    c e e 2 2 2 2 2 2 2 2 4 4 2 2 e 
+    c e e 2 2 2 2 2 2 2 2 2 2 2 2 e 
+    c e e 2 2 2 2 2 2 2 2 2 2 2 2 e 
+    c e e 2 2 2 2 2 2 2 2 2 2 2 2 e 
+    c e e 2 2 2 2 2 2 2 2 2 2 4 2 e 
+    . e e e 2 2 2 2 2 2 2 2 2 4 e . 
+    . 2 e e 2 2 2 2 2 2 2 2 4 2 e . 
+    . . 2 e e 2 2 2 2 2 4 4 2 e . . 
+    . . . 2 2 e e 4 4 4 2 e e . . . 
+    . . . . . 2 2 e e e e . . . . . 
+    `, SpriteKind.Food)
+food1.setPosition(10, 40)
+let food2 = sprites.create(img`
+    . . . . . . . e c 7 . . . . . . 
+    . . . . e e e c 7 7 e e . . . . 
+    . . c e e e e c 7 e 2 2 e e . . 
+    . c e e e e e c 6 e e 2 2 2 e . 
+    . c e e e 2 e c c 2 4 5 4 2 e . 
+    c e e e 2 2 2 2 2 2 4 5 5 2 2 e 
+    c e e 2 2 2 2 2 2 2 2 4 4 2 2 e 
+    c e e 2 2 2 2 2 2 2 2 2 2 2 2 e 
+    c e e 2 2 2 2 2 2 2 2 2 2 2 2 e 
+    c e e 2 2 2 2 2 2 2 2 2 2 2 2 e 
+    c e e 2 2 2 2 2 2 2 2 2 2 4 2 e 
+    . e e e 2 2 2 2 2 2 2 2 2 4 e . 
+    . 2 e e 2 2 2 2 2 2 2 2 4 2 e . 
+    . . 2 e e 2 2 2 2 2 4 4 2 e . . 
+    . . . 2 2 e e 4 4 4 2 e e . . . 
+    . . . . . 2 2 e e e e . . . . . 
+    `, SpriteKind.Food)
+food2.setPosition(150, 60)
+let food3 = sprites.create(img`
+    . . . . . . . e c 7 . . . . . . 
+    . . . . e e e c 7 7 e e . . . . 
+    . . c e e e e c 7 e 2 2 e e . . 
+    . c e e e e e c 6 e e 2 2 2 e . 
+    . c e e e 2 e c c 2 4 5 4 2 e . 
+    c e e e 2 2 2 2 2 2 4 5 5 2 2 e 
+    c e e 2 2 2 2 2 2 2 2 4 4 2 2 e 
+    c e e 2 2 2 2 2 2 2 2 2 2 2 2 e 
+    c e e 2 2 2 2 2 2 2 2 2 2 2 2 e 
+    c e e 2 2 2 2 2 2 2 2 2 2 2 2 e 
+    c e e 2 2 2 2 2 2 2 2 2 2 4 2 e 
+    . e e e 2 2 2 2 2 2 2 2 2 4 e . 
+    . 2 e e 2 2 2 2 2 2 2 2 4 2 e . 
+    . . 2 e e 2 2 2 2 2 4 4 2 e . . 
+    . . . 2 2 e e 4 4 4 2 e e . . . 
+    . . . . . 2 2 e e e e . . . . . 
+    `, SpriteKind.Food)
+food3.setPosition(10, 80)
+let food4 = sprites.create(img`
+    . . . . . . . e c 7 . . . . . . 
+    . . . . e e e c 7 7 e e . . . . 
+    . . c e e e e c 7 e 2 2 e e . . 
+    . c e e e e e c 6 e e 2 2 2 e . 
+    . c e e e 2 e c c 2 4 5 4 2 e . 
+    c e e e 2 2 2 2 2 2 4 5 5 2 2 e 
+    c e e 2 2 2 2 2 2 2 2 4 4 2 2 e 
+    c e e 2 2 2 2 2 2 2 2 2 2 2 2 e 
+    c e e 2 2 2 2 2 2 2 2 2 2 2 2 e 
+    c e e 2 2 2 2 2 2 2 2 2 2 2 2 e 
+    c e e 2 2 2 2 2 2 2 2 2 2 4 2 e 
+    . e e e 2 2 2 2 2 2 2 2 2 4 e . 
+    . 2 e e 2 2 2 2 2 2 2 2 4 2 e . 
+    . . 2 e e 2 2 2 2 2 4 4 2 e . . 
+    . . . 2 2 e e 4 4 4 2 e e . . . 
+    . . . . . 2 2 e e e e . . . . . 
+    `, SpriteKind.Food)
+food4.setPosition(150, 100)
+let enemy1 = sprites.create(img`
+    . . . . f f f f f f . . . . . . 
+    . . . f 2 f e e e e f f . . . . 
+    . . f 2 2 2 f e e e e f f . . . 
+    . . f e e e e f f e e e f . . . 
+    . f e 2 2 2 2 e e f f f f . . . 
+    . f 2 e f f f f 2 2 2 e f . . . 
+    . f f f e e e f f f f f f f . . 
+    . f e e 4 4 f b e 4 4 e f f . . 
+    . . f e d d f 1 4 d 4 e e f . . 
+    . . . f d d d d 4 e e e f . . . 
+    . . . f e 4 4 4 e e f f . . . . 
+    . . . f 2 2 2 e d d 4 . . . . . 
+    . . . f 2 2 2 e d d e . . . . . 
+    . . . f 5 5 4 f e e f . . . . . 
+    . . . . f f f f f f . . . . . . 
+    . . . . . . f f f . . . . . . . 
+    `, SpriteKind.Enemy)
+enemy1.setPosition(150, 40)
+let enemy2 = sprites.create(img`
+    . . . . . . . . . . . . . . . . 
+    . . . . . . f f f f f f . . . . 
+    . . . . f f e e e e f 2 f . . . 
+    . . . f f e e e e f 2 2 2 f . . 
+    . . . f e e e f f e e e e f . . 
+    . . . f f f f e e 2 2 2 2 e f . 
+    . . . f e 2 2 2 f f f f e 2 f . 
+    . . f f f f f f f e e e f f f . 
+    . . f f e 4 4 e b f 4 4 e e f . 
+    . . f e e 4 d 4 1 f d d e f . . 
+    . . . f e e e 4 d d d d f . . . 
+    . . . . 4 d d e 4 4 4 e f . . . 
+    . . . . e d d e 2 2 2 2 f . . . 
+    . . . . f e e f 4 4 5 5 f f . . 
+    . . . . f f f f f f f f f f . . 
+    . . . . . f f . . . f f f . . . 
+    `, SpriteKind.Enemy)
+enemy2.setPosition(10, 60)
+let enemy3 = sprites.create(img`
+    . . . . f f f f f f . . . . . . 
+    . . . f 2 f e e e e f f . . . . 
+    . . f 2 2 2 f e e e e f f . . . 
+    . . f e e e e f f e e e f . . . 
+    . f e 2 2 2 2 e e f f f f . . . 
+    . f 2 e f f f f 2 2 2 e f . . . 
+    . f f f e e e f f f f f f f . . 
+    . f e e 4 4 f b e 4 4 e f f . . 
+    . . f e d d f 1 4 d 4 e e f . . 
+    . . . f d d d d 4 e e e f . . . 
+    . . . f e 4 4 4 e e f f . . . . 
+    . . . f 2 2 2 e d d 4 . . . . . 
+    . . . f 2 2 2 e d d e . . . . . 
+    . . . f 5 5 4 f e e f . . . . . 
+    . . . . f f f f f f . . . . . . 
+    . . . . . . f f f . . . . . . . 
+    `, SpriteKind.Enemy)
+enemy3.setPosition(150, 80)
+let enemy4 = sprites.create(img`
+    . . . . . . . . . . . . . . . . 
+    . . . . . . f f f f f f . . . . 
+    . . . . f f e e e e f 2 f . . . 
+    . . . f f e e e e f 2 2 2 f . . 
+    . . . f e e e f f e e e e f . . 
+    . . . f f f f e e 2 2 2 2 e f . 
+    . . . f e 2 2 2 f f f f e 2 f . 
+    . . f f f f f f f e e e f f f . 
+    . . f f e 4 4 e b f 4 4 e e f . 
+    . . f e e 4 d 4 1 f d d e f . . 
+    . . . f e e e 4 d d d d f . . . 
+    . . . . 4 d d e 4 4 4 e f . . . 
+    . . . . e d d e 2 2 2 2 f . . . 
+    . . . . f e e f 4 4 5 5 f f . . 
+    . . . . f f f f f f f f f f . . 
+    . . . . . f f . . . f f f . . . 
+    `, SpriteKind.Enemy)
+enemy4.setPosition(10, 100)
+enemy1.follow(food1, 15)
+enemy2.follow(food2, 30)
+enemy3.follow(food3, 25)
+enemy4.follow(food4, 20)
